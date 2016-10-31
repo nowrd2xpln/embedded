a072a24cd6f77fb35cbe9600a4ebb0f289f3fb58
Allen Mamaril <allen.mamaril@gmail.com>
2016-10-30 20:06:55 -0700

    Enable SPI interrupt, clean up code, add SPI ISR, enable Global Interrupts, add SPI_Tranceiver()
    Enable SPI interrupt, clean up code, add SPI ISR, enable Global
    Interrupts, add SPI_Tranceiver()

--- avr/atmega16/spi_driver/spi_driver.c
+++ avr/atmega16/spi_driver/spi_driver.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define F_CPU	8000000UL	//--- CPU Clock Frequency 
 
@@ -7,35 +8,50 @@
 #endif /* NULL */
 
 /* SPI bits */
-#ifndef SCK
-#define SCK		7
-#endif /* SCK */
-
-#ifndef MISO
-#define MISO	6
-#endif /* MISO */
-
-#ifndef MOSI
-#define MOSI	5
-#endif /* MOSI */
-
-#ifndef nSS
-#define nSS		4
-#endif /* nSS */
-
+#define SCK		DDB7
+#define MISO	DDB6
+#define MOSI	DDB5
+#define nSS		DDB4
 
 /* Prototype Declaration */
 void SPI_MasterInit(void);
+unsigned char SPI_Tranceiver(unsigned char data);
+ISR(SPI_STC_vect);
+
+/* Global variables */
+volatile unsigned char data;
 
 int main(void)
 {
 	SPI_MasterInit();
+
+	while(1)
+	{
+
+	}
 }
 
 /* Proto Type Declaration */
 void SPI_MasterInit(void)
 {
-	DDRB |= (1<<SCK)|(1<<MOSI)|(1<<nSS);	//-- SCK, MOSI and nSS as outputs
-	DDRB &= ~(1<<MISO);						//-- MISO as input
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);	//-- Enable SPI, Master, set clock rate fck/16
+	DDRB |= (1<<SCK)|(1<<MOSI)|(1<<nSS);			//-- SCK, MOSI and nSS as outputs
+	DDRB &= ~(1<<MISO);								//-- MISO as input
+	SPCR  = (1<<SPE)|(1<<SPIE)|(1<<MSTR)|(1<<SPR0);	//-- Enable SPI, SPI Interrupt, Master, set clock rate fck/16
+
+	sei();	//-- Enable Global Interrupts
+}
+
+ISR (SPI_STC_vect)
+{
+	data = SPDR;
+	// Process the received data
+}
+
+unsigned char SPI_Tranceiver(unsigned char data)
+{
+    SPDR = data;				//-- Load data into the buffer
+
+    while(!(SPSR & (1<<SPIF) ));//-- Wait until transmission complete
+
+    return(SPDR);				//-- Return received data
 }
